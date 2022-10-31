f7d68a6291002d8fede59fc8e7a6d85beac2c38a
Anthony Barrera <55001338+AnthonyBarrera116@users.noreply.github.com>
2022-10-31 18:30:58 -0400

    Add files via upload

---
+++ Sub/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text, View } from "react-native";
+import MainScreen from "./Screens/MainScreen";
+
+export default function App(){
+    return (
+        <MainScreen/>
+    );
+}
