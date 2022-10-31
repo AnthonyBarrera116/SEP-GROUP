1f4932cc803fb14afb0d4c6b7b2b648f0f9b7ce2
Anthony Barrera <55001338+AnthonyBarrera116@users.noreply.github.com>
2022-10-31 18:34:35 -0400

    Delete Fully working directory

--- Fully working/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Text, View } from "react-native";
-import MainScreen from "./Screens/MainScreen";
-
-export default function App(){
-    return (
-        <MainScreen/>
-    );
-}
