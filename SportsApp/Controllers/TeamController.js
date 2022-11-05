77173ce838837b97d0dea0cc0fc44e2239acc171
er-henson <tilvunun@gmail.com>
2022-11-05 14:47:39 -0400

    added team functionality, coach modification, player request controller

--- SportsApp/Controllers/TeamController.js
+++ SportsApp/Controllers/TeamController.js
@@ -81,3 +81,151 @@ exports.addPlayer = async function(request, response)
         response.send(null);
     }
 }
+
+/*
+function to remove a non-coach player from the team
+trying to remove the coach will result in response being 'null' and nothing happening
+assumes request contains
+    player ID of player to be removed
+    team ID of team to remove player from
+responds with the full team information after player is removed
+*/
+exports.removePlayer = async function(request, response)
+{
+    let playerID = request.body.playerID;
+    let teamID = request.body.teamID;
+    
+    // read team information from DAO
+    let team = await dao.read(teamID);
+    
+    // if read succeeds
+    if(team !== null)
+    {
+        // find and remove the player from the list of playerIDs
+        let i = 0;
+        let removeIndex = -1;
+        while ( i < team.PlayerIDs.length )
+        {
+            // if we find the player to remove, we break out of the loop
+            if( team.PlayerIDs[i] === playerID )
+            {
+                removeIndex = i;
+                break;
+            }
+            i++;
+        }
+        // if removeIndex is set properly
+        if( removeIndex >= 0 )
+        {
+            // splice the list, remove the player that the index is at. the '1' is saying how many elements to remove, which is just 1 in this case
+            team.PlayerIDs.splice(removeIndex, 1);
+        }  
+        
+        // update the DAO with the new list of playerIDs (with the player removed)
+        let returnedTeam = await dao.update(team);
+        
+        // respond with new team information after the update
+        if(returnedTeam !== null)
+        {
+            response.status(200);
+            response.send(team);
+        }
+        else // if the returned team is 'null', respond with 'null'
+        {
+            response.status(500);
+            response.send(null)
+        }
+    }
+    else // if read fails, set status to 500 and return null
+    {
+        response.status(500);
+        response.send(null);
+    }
+}
+
+/*
+function to remove a coach from a team
+assumes request contains
+    teamID
+responds with team information after coach removal
+NOTE - there is a function in the userController that updates coach statuses. that function must be called with this one on the frontend, as this file is not concered with individual user-side happenings
+*/
+exports.removeCoach = async function(request, response)
+{
+    // get the teamID from the body
+    let teamID = request.body.teamID;
+    
+    // get the team information from the DAO
+    let team = await dao.read(teamID);
+    
+    // if reading the team is successful
+    if(team !== null)
+    {
+        // set the coach to 'null'
+        team.CoachID = null;
+        
+        // send the update to the DAO
+        let returnedTeam = await dao.update(team);
+        
+        // if the update succeeds, respond with updated team information. else, respond with null
+        if(returnedTeam !== null)
+        {
+            response.status(200);
+            response.send(returnedTeam);
+        }
+        else // if the update fails, respond with null
+        {
+            response.status(500);
+            response.send(null);
+        }
+    }
+    else // if reading the team fails, respond with null
+    {
+        response.status(404);
+        response.send(null);
+    }
+}
+
+/*
+function to make a player a coach for a team
+assumes request contains
+    teamID
+    playerID of player to be made a coach
+responds with full team information after coach update
+NOTE - there is a function in the userController that updates coach statuses. that function must be called with this one on the frontend, as this file is not concered with individual user-side happenings
+*/
+exports.makeCoach = async function()
+{
+    // get the teamID and playerID from the request body
+    let teamID = request.body.teamID;
+    let playerID = request.body.playerID;
+    
+    // get the team information from the DAO
+    let team = await dao.read(teamID);
+    
+    if(team !== null) // reading the team succeeds, update the coachID
+    {
+        // update the Coach information
+        team.CoachID = playerID;
+        
+        // send the update to the DAO
+        let returnedTeam = await dao.update(team);
+        
+        // if the update succeeds, return updated team information
+        if( returnedTeam !== null )
+        {
+            response.status(200);
+            response.send(returnedTeam);
+        }
+        else // else return null
+        {
+            response.status(500);
+            response.send(null);
+        }
+    }
+    else // if reading the team fails, send 'null'
+    {
+        response.status(404);
+        response.send(null);
+    }
+}
